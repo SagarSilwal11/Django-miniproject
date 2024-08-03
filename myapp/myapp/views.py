10d9c9a035ed209009b6abd970c3c13d36bcf0bb
SagarSilwal11 <sagar.silwal002@gmail.com>
2024-08-04 02:12:14 +0545

    django_v1sapp

---
+++ myapp/myapp/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def homepage(request):
+    return render(request,'home.html')
+
+def aboutpage(request):
+    return render(request,'about.html')
---
+++ myapp/posts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Posts
+from django.contrib.auth.decorators import login_required
+from .import forms
+# Create your views here.
+def posts_list(request):
+    posts=Posts.objects.all().order_by('-date')
+    return render(request,'posts/posts_list.html',{'posts':posts}) # here {'posts':posts } is data passed as third parameter
+
+def post_page(request,slug):
+    post=Posts.objects.get(slug=slug)
+    return render(request,'posts/posts_page.html',{'post':post})
+
+
+@login_required(login_url="/users/login")
+def post_new(request):
+    if request.method=="POST":
+        form=forms.CreatePost(request.POST,request.FILES)
+        if form.is_valid():
+            newpost=form.save(commit=False)# save with the user
+            newpost.author=request.user
+            newpost.save()
+            return redirect('posts:list')
+    form=forms.CreatePost()
+    return render(request,'posts/post_new.html',{'form':form})
